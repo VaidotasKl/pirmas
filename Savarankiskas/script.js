a4d0e2732e44057b0d8de2ad76c3e6dc818a34f4
VaidotasKl <vaidotas.klicius@gmail.com>
2024-11-24 13:06:24 +0200

    Advokato pradzia

--- Savarankiskas/script.js
+++ Savarankiskas/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.title = "nesigauna";
-    document.getElementById("manas").innerHTML = "naujas tekstas";
+        
+        document.getElementById("manas").innerHTML = "naujas tekstas";
     // document.getElementById("removable").innerHTML = "";
 
 
