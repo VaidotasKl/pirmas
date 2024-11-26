6a400b705bf0a44f6543b3e82eb3b1e61587efda
VaidotasKl <vaidotas.klicius@gmail.com>
2024-11-26 19:35:20 +0200

    Advokatas

--- Savarankiskas/script.js
+++ Savarankiskas/script.js
@@ -1,7 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
+console.log("bandom");
+// document.getElementById("siusti").onclick=function (event) {
+    
+//     console.log("Button clicked!");
+// }
 
-document.getElementsByType("submit").onClick=function () {
-    PostMessage("Button clicked!");}
+document.querySelector(".Forma").onsubmit=function (event) {
+    event.preventDefault();
+        alert("Jūsų užklausa gauta!");} 
         
 });
 
